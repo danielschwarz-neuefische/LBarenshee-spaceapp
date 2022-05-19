faa36e2b1411f3993a6beabb3acb92f868f5f1a5
LBarenschee <barenschee@gmail.com>
2022-05-19 15:49:47 +0200

    created test for apiController currently a problem with the placeholder for the API KEY. Test is not running.

--- backend/src/main/java/de/neuefische/backend/model/NasaPicture.java
+++ backend/src/main/java/de/neuefische/backend/model/NasaPicture.java
@@ -2,12 +2,14 @@ package de.neuefische.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class NasaPicture {
 
     private String title;
--- backend/src/test/java/de/neuefische/backend/controller/APIControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/APIControllerTest.java
@@ -1,7 +1,44 @@
 package de.neuefische.backend.controller;
 
+import de.neuefische.backend.model.NasaPicture;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class APIControllerTest {
 
+
+    @Autowired
+    private WebTestClient testClient;
+
+
+
+    @Test
+    void getPictureOfTheDay() {
+        //WHEN
+        NasaPicture actual = testClient.get()
+                .uri("/picoftheday")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(NasaPicture.class)
+                .returnResult()
+                .getResponseBody();
+
+        //THEN
+        NasaPicture expected = NasaPicture.builder()
+                .title(actual.getTitle())
+                .date(actual.getDate())
+                .explanation(actual.getExplanation())
+                .copyright(actual.getCopyright())
+                .url(actual.getUrl())
+                .hdurl(actual.getHdurl())
+                .mediaType(actual.getMediaType())
+                .serviceVersion(actual.getServiceVersion())
+                .build();
+        assertEquals(expected, actual);
+    }
 }
